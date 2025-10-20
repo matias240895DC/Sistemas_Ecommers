d394739017bc19c84a0713fa5e23dac85b930ff8
pablomaximiliano97a <pablodocquimica@gmail.com>
2025-10-20 11:24:42 -0300

    Reestructuracion del formulario nuevo-registro, y generacion de estilos para pantalla de usuario-creado. Enrutamiento de componentes para interconectarlos.

--- clients/src/app/app.routes.ts
+++ clients/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/auth/login/login';
+import { NuevoRegistro } from './auth/auth/nuevo-registro/nuevo-registro';
+import { UsuarioCreado } from './auth/auth/usuario-creado/usuario-creado';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login,
+    },
+    {
+        path: 'registro',
+        component: NuevoRegistro,
+    },
+    {
+        path: 'usuario-creado',
+        component: UsuarioCreado,
+    }
+];
