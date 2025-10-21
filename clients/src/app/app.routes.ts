931b9ec662636080bdfc2ab13d023a46dfb26b57
pablomaximiliano97a <pablodocquimica@gmail.com>
2025-10-21 17:30:54 -0300

    Creando una rama nueva (feature/landingPage, y configurando estructura de carpetas con algunas rutas

--- clients/src/app/app.routes.ts
+++ clients/src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { RecuperarContra } from './auth/auth/recuperar-contra/recuperar-contra';
 import { EnlaceEnviado } from './auth/auth/enlace-enviado/enlace-enviado';
 import { RestablecerContra } from './auth/auth/restablecer-contra/restablecer-contra';
 import { ContraRestablecida } from './auth/auth/contra-restablecida/contra-restablecida';
+import { Landing } from './pages/landing/landing';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'landing', pathMatch: 'full' },
+    { path: 'landing', component: Landing },
     { path: 'login', component: Login },
     { path: 'registro', component: NuevoRegistro },
     { path: 'usuario-creado', component: UsuarioCreado },
