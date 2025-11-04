0e72824214583271903c8d6a0e27e8c1ae1c00a7
pablomaximiliano97a <pablodocquimica@gmail.com>
2025-11-04 12:35:35 -0300

    Nuevo cambios en dashboard

---
+++ clients/src/app/pages/dashboard/features/gestion-datos/paises/paises.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paises',
+  imports: [],
+  templateUrl: './paises.html',
+  styleUrl: './paises.css'
+})
+export class Paises {
+
+}
