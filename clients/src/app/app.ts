1f25c4516bbc814cfed259dd4e6c5055ab534bf2
pablomaximiliano97a <pablodocquimica@gmail.com>
2025-11-07 10:47:37 -0300

    Comiteando cambios

--- clients/src/app/app.ts
+++ clients/src/app/app.ts
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import { Component, effect, inject, Renderer2, Signal, Inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ThemeService } from './services/theme.service';
-import { DOCUMENT } from '@angular/common';
-=======
-import { Component, effect, inject, Renderer2 } from '@angular/core';
+import { Component, DOCUMENT, effect, inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/theme.service';
 import { PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
->>>>>>> a80fb505b6aef3328644c1529328069da5c2d98b
 
 @Component({
   selector: 'app-root',
@@ -25,7 +18,6 @@ export class App {
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     effect(() => {
-<<<<<<< HEAD
       const body = this.document?.body; // ✅ usamos el documento inyectado
       if (!body) return; // seguridad extra
 
@@ -33,16 +25,15 @@ export class App {
         this.renderer.addClass(body, 'dark');
       } else {
         this.renderer.removeClass(body, 'dark');
-=======
-      // Solo ejecutar en el navegador (no en SSR)
-      if (isPlatformBrowser(this.platformId)) {
-        if (this.themeService.darkMode()) {
-          this.renderer.addClass(document.body, 'dark');
-        } else {
-          this.renderer.removeClass(document.body, 'dark');
-        }
->>>>>>> a80fb505b6aef3328644c1529328069da5c2d98b
       }
     });
   }
 }
+
+//  if (isPlatformBrowser(this.platformId)) {
+//         if (this.themeService.darkMode()) {
+//           this.renderer.addClass(document.body, 'dark');
+//         } else {
+//           this.renderer.removeClass(document.body, 'dark');
+//         }
+//       }
